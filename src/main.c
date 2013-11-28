c9d0508f417eb38c05659a5ddc9545c9ad44f6b6
jeff <knifed@gmail.com>
2013-11-28 19:43:52 +0000

    working elevator simulation

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <math.h>
 #include <string.h>
+#include "main.h"
 
 const int SECONDS = 10.0;
 double START;
@@ -17,69 +18,10 @@ typedef struct {
 
 } Person;
 
-enum State {New, Work, Move, Gone, Read, Write};
-int id;
-double t;
+enum State { Work, Elevator, Waiting };
 
-double rand_range(double min, double max) {
-
-  return (double)rand()/RAND_MAX * (max - min) + min;
-
-}
-
-// return value, but not the number exclude
-double rand_range_ex(int ex, int min, int max) {
-
-  int random = ex;
-
-  while (random == ex) {
-    random = rand_range(min, max);
-  }
-
-  return random;
-}
-
-
-Person newPerson(int new_id) {
-  Person person;
-  person.id = new_id;
-  person.state = New;
-  return person;
-}
-
-void start_working(Person person) {
-
-  t = MPI_Wtime() + rand_range(1, 4);
-  person.state = Work;
-
-}
-
-void Simulate(Person person) {
-
-  if (person.state == Work) {
-    int now = MPI_Wtime();
-    if (t < now) {
-      person.state = Move;
-    }
-  }
-}
-
-int go_to_next_floor(int current_floor) {
-
-  int random_value = current_floor;
-  while (random_value == current_floor) {
-    random_value = 2 + rand() % 3;
-  }
-  return random_value;
-}
-
-
-void Elevator(int rank, MPI_Comm comm) {
-
-}
-
-void Floor(int rank, MPI_Comm comm) {
-
+int rand_num(int max) {
+	return (rand() % max) + 1;
 }
 
 int main(int argc, char **argv) {
@@ -108,6 +50,7 @@ int main(int argc, char **argv) {
   printf("%d/%d achieved the file_open result: %d.\n", rank+1, size, rc);
 
   // set the individual pointer to our position in the config file
+  // master is the master of elevators
   rc = MPI_File_seek(configFile, rank * nameLength, MPI_SEEK_SET);
  
   rc = MPI_File_read(configFile, buf, nameLength, MPI_CHAR, &status);
@@ -127,10 +70,108 @@ int main(int argc, char **argv) {
 
   // we check whether or not we are the root process
   if (rank == root) {
-    // we do root-y stuff =)
+	  master(rank);
+  }
+  else {
+	  worker(rank, ourname);
   }
  
   printf("%d/%d ended.\n", rank+1, size);
 
   MPI_Finalize();
+}
+
+const int MAX_FLOOR = 3;
+
+void master(int rank) {
+	srand(time(NULL));
+	
+	//master manages elevators
+	int floor = 1;
+	bool up = true;
+	
+	while (true) {
+		if (up) {
+			//going up
+			if (floor + 1 <= MAX_FLOOR) {
+				floor++;
+			}
+			else {
+				//at max floor, now going down.
+				up = false;
+				floor--;
+			}
+		}
+		else {
+			//going down
+			if (floor - 1 >= 1) {
+				floor--;
+			}
+			else {
+				//at ground floor, go back up
+				up = true;
+				floor++;
+			}
+		}
+
+		printf("elevator now on floor %d\n", floor);
+		//tell workers what floor we're on
+		MPI_Bcast(&floor, 1, MPI_INT, rank, MPI_COMM_WORLD);
+		
+		//elevator very fast, only 3 seconds to get in or out
+		//also possibly dangerous to occupants.
+		sleep(5);
+	}
+}
+
+/**
+ * Rank is processor number, name is the name of the person toiling away.
+ */
+void worker(int rank, char* name) {
+	srand(time(NULL));
+	
+	int workTicks = rand_num(5);
+	int currWorkTick = 0;
+	int myFloor = rand_num(3);
+	int desiredFloor = myFloor;
+	enum State state = Work;
+	
+	while (true) {
+		int floorOfElevator = 0;
+
+		//receive what floor the elevator is on
+		MPI_Bcast(&floorOfElevator, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+		if (state == Elevator) {
+			printf("%d is riding the elevator to floor %d\n", rank, desiredFloor);
+			
+			//are we on the right floor? if so, state becomes work
+			if (floorOfElevator == desiredFloor) {
+				printf("%d got off elevator and is now working on floor %d\n", rank, desiredFloor);
+				state = Work;
+				myFloor = desiredFloor;
+			}
+		}
+		else if (state == Work) {
+			//do work
+			currWorkTick++;
+			printf("%d did some work on floor %d [%d/%d]\n", rank, myFloor, currWorkTick, workTicks);
+			
+			//receive our next task and wait for the elevator
+			if (currWorkTick >= workTicks) {
+				printf("%d is done with work on floor %d\n", rank, myFloor);
+				currWorkTick = 0;
+				workTicks = rand_num(5);
+				desiredFloor = rand_num(3);
+				state = Waiting;
+			}
+		}
+		else if (state == Waiting) {
+			printf("%d is waiting for the elevator to go to floor %d from floor %d\n", rank, desiredFloor, myFloor);
+			if (floorOfElevator == myFloor) {
+				printf("%d got on the elevator to go to floor %d\n", rank, desiredFloor);
+				state = Elevator;
+			}
+		}
+	}
 }
---
+++ src/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+void master(int rank);
+void worker(int rank, char* name);
+#endif
